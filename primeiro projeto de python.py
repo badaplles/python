993fb79f4e505ae054d4e886dc39f62ef845f78a
badaplles <appettitefordestruction@gmail.com>
2021-09-15 12:18:37 -0300

    primeiro commit

---
+++ primeiro projeto de python.py
@@ -0,0 +1,8 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:\Users\laine\Desktop\script bythhon\condicao if igual a se.py ===
+digite um numero de 1 a 10 :7
+digite outro numero que somado sera igual a 20 :9
+vc errou sua burra kkkk
+>>> 
